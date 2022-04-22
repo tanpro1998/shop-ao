fbbbfea5351eb67bd73969e709bf1025fda8b00e
root <root@TAN.localdomain>
2022-04-22 12:45:45 +0700

    commit

--- admin/src/pages/Home/Home.jsx
+++ admin/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Chart from "../../components/chart/Chart";
 import Features from "../../components/Features/Features";
 import Navbar from "../../components/Navbar/Navbar";
