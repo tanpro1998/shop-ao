a6c02b91ca72d1e60b3862cd634bf477b433b593
Tan <davidtanpro1998@gmail.com>
2022-04-25 16:31:27 +0700

    commit

--- frontend/src/pages/Accessory/Accessories.jsx
+++ frontend/src/pages/Accessory/Accessories.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import Accessory from "../../components/Accessory/Accessory";
 import Grid from "../../components/Grid/Grid";
 import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer"
+import Footer from "../../components/Footer/Footer";
 import Helmet from "../../components/Helmet/Helmet";
 import Section, {
   SectionBody,
@@ -32,7 +32,7 @@ const Accessories = () => {
     <Helmet title="Phụ Kiện">
       <Header />
       <Section>
-        <SectionTitle className="border-bottom">Top Sản Phẩm</SectionTitle>
+        <SectionTitle>Top Sản Phẩm</SectionTitle>
         <SectionBody>
           <Grid col={4} mdCol={2} smCol={1} gap={20}>
             {totalAccessories.slice(0, 4).map((item, index) => (
@@ -54,7 +54,7 @@ const Accessories = () => {
           <Link to="/">
             <div className="banner">
               <img
-                src="https://bizweb.dktcdn.net/100/269/201/themes/604642/assets/slide-img3.jpg?1621590443633"
+                src="https://adimanav.com/wp-content/uploads/2018/02/head-turner-graphic-tshirt-banner-60.jpg"
                 alt=""
                 className=""
               />
@@ -66,17 +66,20 @@ const Accessories = () => {
         <SectionTitle>Sản Phẩm Mới</SectionTitle>
         <SectionBody>
           <Grid col={4} mdCol={2} smCol={1} gap={20}>
-            {totalAccessories?.slice(0, 8).map((item, index) => (
-              <Accessory
-                key={index}
-                img01={item.image01}
-                img02={item.image02}
-                name={item.title}
-                price={Number(item.price)}
-                slug={item.slug}
-                id={item._id}
-              />
-            ))}
+            {totalAccessories
+              ?.slice(0, 8)
+              .sort((a, b) => a.createdAt - b.createdAt)
+              .map((item, index) => (
+                <Accessory
+                  key={index}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                  id={item._id}
+                />
+              ))}
           </Grid>
         </SectionBody>
       </Section>
@@ -84,17 +87,20 @@ const Accessories = () => {
         <SectionTitle>Sản phẩm bán chạy</SectionTitle>
         <SectionBody>
           <Grid col={4} mdCol={2} smCol={1} gap={20}>
-            {totalAccessories?.slice(0, 12).map((item, index) => (
-              <Accessory
-                key={index}
-                img01={item.image01}
-                img02={item.image02}
-                name={item.title}
-                price={Number(item.price)}
-                slug={item.slug}
-                id={item._id}
-              />
-            ))}
+            {totalAccessories
+              ?.slice(0, 12)
+              .sort((a, b) => a.price - b.price)
+              .map((item, index) => (
+                <Accessory
+                  key={index}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                  id={item._id}
+                />
+              ))}
           </Grid>
         </SectionBody>
       </Section>
